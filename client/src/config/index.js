c7964a475103a987609fec72d681053089b0d2de
Bertrand Steinmetz <bstenm@hotmail.com>
2019-01-13 09:34:08 -0500

    cosmetics

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -3,6 +3,6 @@ const env = process.env.NODE_ENV;
 const grapqhQLendPoint =
       env === 'development'
             ? 'http://localhost:4000'
-            : 'https://ico-contributions-visualizer.herokuapp.com/';
+            : 'https://ico-contributions-visualization.herokuapp.com/';
 
 export default { grapqhQLendPoint };
