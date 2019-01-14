220cb357da12b285a4c5e77d8866870a66fc0975
Bertrand Steinmetz <bstenm@hotmail.com>
2019-01-14 00:07:57 -0500

    cosmetics

--- client/src/components/DataVisualization/DataVisualizationContainer.test.js
+++ client/src/components/DataVisualization/DataVisualizationContainer.test.js
@@ -35,41 +35,39 @@ const data = [
       },
 ];
 
-describe('(Container) DataVisualization', () => {
-      let props;
-      let wrapper;
+let props;
+let wrapper;
 
-      beforeEach(() => {
-            props = {
-                  data,
-                  range: [1000, 3000],
-                  currencyList: ['BTC', 'ETH'],
-                  setMaxValue: jest.fn(),
-            };
-            wrapper = shallow(<DataVisualizationContainer {...props} />);
+beforeEach(() => {
+      props = {
+            data,
+            range: [1000, 3000],
+            currencyList: ['BTC', 'ETH'],
+            setMaxValue: jest.fn(),
+      };
+      wrapper = shallow(<DataVisualizationContainer {...props} />);
+});
+
+it('Calls an action to set the range max in redux store', () => {
+      expect(props.setMaxValue).toHaveBeenCalledTimes(1);
+      expect(props.setMaxValue).toHaveBeenCalledWith(20000);
+});
+
+it('Displays a DataVisualization component', () => {
+      expect(wrapper.find(DataVisualization)).toHaveLength(1);
+});
+
+it('Passes the data filtered through the range and currency filters to the DataVisualization component', () => {
+      expect(wrapper.find(DataVisualization).props(data)).toEqual({
+            data: [
+                  { currency: 'BTC', value: 3000 },
+                  { currency: 'ETH', value: 1500 },
+            ],
       });
+});
 
-      it('Calls an action to set the range max in redux store', () => {
-            expect(props.setMaxValue).toHaveBeenCalledTimes(1);
-            expect(props.setMaxValue).toHaveBeenCalledWith(20000);
-      });
+it('Does not display a DataVisualization component if no data left for the range selected', () => {
+      wrapper.setProps({ range: [0, 200] });
 
-      it('Displays a DataVisualization component', () => {
-            expect(wrapper.find(DataVisualization)).toHaveLength(1);
-      });
-
-      it('Passes the data filtered through the range and currency filters to the DataVisualization component', () => {
-            expect(wrapper.find(DataVisualization).props(data)).toEqual({
-                  data: [
-                        { currency: 'BTC', value: 3000 },
-                        { currency: 'ETH', value: 1500 },
-                  ],
-            });
-      });
-
-      it('Does not display a DataVisualization component if no data left for the range selected', () => {
-            wrapper.setProps({ range: [0, 200] });
-
-            expect(wrapper.find(DataVisualization)).toHaveLength(0);
-      });
+      expect(wrapper.find(DataVisualization)).toHaveLength(0);
 });
